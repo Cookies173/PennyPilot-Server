49186dd6da8e8bea5c85b531c4877ba4695b3654
Cookies173 <meet.kotadiya.mst21@itbhu.ac.in>
2025-12-03 20:18:50 +0530

    Fix startOfMonth calculation in dashController
    Adjust startOfMonth to use the last day of the previous month.

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -142,7 +142,7 @@ export const getBudget = async (req, res) => {
         );
 
         const currentDate = new Date();
-        const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+        const startOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
         const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth()+1, 0);
 
         const expenses = await db.query(`
